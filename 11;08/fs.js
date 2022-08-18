a4a4af35988b5716c3c29037ecb037f94f6dad1a
Glieberson <105432927+Gliberson00@users.noreply.github.com>
2022-08-18 15:16:00 -0300

    Add files via upload

---
+++ 11;08/fs.js
@@ -0,0 +1,50 @@
+//const { error } = require('console');
+//const fs =  require('fs');
+
+
+
+//if(!fs.existsSync('./public')){
+ //   fs.mkdirSync('./public', (err)=>{
+    //  //   //    throw err;
+       // }
+       // console.log('pasta criada com sucesso!');
+
+    //});
+//}else{
+    //console.log('a pasta ja existe');
+//}
+//fs.existsSync('/.public') || fs.existsSync('./public');
+
+
+//if(fs.existsSync('./public')){
+   // fs.renameSync('./public','./dimitri', (err)=>{
+      //  if(err){
+       //      throw err;
+       // }
+      //  console.log('pasta renomeada com sucesso!');
+
+    //});
+//}else{//
+   // console.log('Impossivel renomear porque "public" não existe"');
+//}
+
+//if(fs.existsSync('./dimitri')){
+   // fs.rmdir('./dimitri',{recursive:true},(err)=>{
+       // if(err){
+             throw err;
+        //}
+        //console.log('pasta deletada com sucesso!');
+
+   // });
+//}else{//
+    //console.log('Impossivel deletar dimitri porque "public" não existe"');
+//}
+
+if(fs.existsSync('teste.txt')){
+fs.writeFile('teste.txt','teste de conteudo!',(err)=>{
+    if(err){
+        throw err;
+    }
+    console.log('Arquivo criado com sucesso!');
+})
+}
